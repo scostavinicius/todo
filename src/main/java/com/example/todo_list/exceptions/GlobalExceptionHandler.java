7caa09e2a295b6419ffbb2c4810df64e7cb67f66
scostavinicius <2003.costa.vinicius@gmail.com>
2025-02-07 00:41:03 -0300

    Tratamento de exceções genéricas

--- src/main/java/com/example/todo_list/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo_list/exceptions/GlobalExceptionHandler.java
@@ -17,4 +17,9 @@ public class GlobalExceptionHandler extends RuntimeException {
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno no servidor.");
+    }
 }
